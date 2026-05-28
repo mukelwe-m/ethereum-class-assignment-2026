58de5d551a2b2781848936894339992db69e3ea0
mukelwe-m <mukelwemdluli@gmail.com>
2026-05-28 13:40:11 +0200

    created two contracts

--- 02-uniswap-v4/packages/hardhat/contracts/FNBToken.sol
+++ 02-uniswap-v4/packages/hardhat/contracts/FNBToken.sol
@@ -1,4 +1,3 @@
-contract FNBToken {}
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.26 <0.9.0;
 
@@ -7,12 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 /// @title FNBToken
 /// @notice ERC20 token representing FNB eBucks with symbol FNBT
 contract FNBToken is ERC20 {
-    /**
-     * @notice Deploys the FNBToken contract
-     * @param initialSupply The initial ERC20 supply to mint to the deployer
-     *
-     * Default ERC20 decimals is 18.
-     */
     constructor() ERC20("FNB Token", "FNBT") {
         _mint(msg.sender, 1_000_000 * 10 ** decimals());
     }
--- 02-uniswap-v4/packages/hardhat/contracts/PNPToken.sol
+++ 02-uniswap-v4/packages/hardhat/contracts/PNPToken.sol
@@ -1,4 +1,3 @@
-contract PNPToken {}
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.26 <0.9.0;
 
@@ -7,12 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 /// @title PNPToken
 /// @notice ERC20 token representing Pick n Pay points with symbol PNPT
 contract PNPToken is ERC20 {
-    /**
-     * @notice Deploys the PNPToken contract
-     * @param initialSupply The initial ERC20 supply to mint to the deployer
-     *
-     * Default ERC20 decimals is 18.
-     */
     constructor() ERC20("PNP Token", "PNPT") {
         _mint(msg.sender, 1_000_000 * 10 ** decimals());
     }
