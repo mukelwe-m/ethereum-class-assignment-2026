ed13e282d7552ac3ae657192061cb563e49ef9fd
mukelwe-m <mukelwemdluli@gmail.com>
2026-05-28 13:49:23 +0200

    ensured comments made sense

--- 02-uniswap-v4/packages/hardhat/contracts/FNBToken.sol
+++ 02-uniswap-v4/packages/hardhat/contracts/FNBToken.sol
@@ -6,7 +6,13 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 /// @title FNBToken
 /// @notice ERC20 token representing FNB eBucks with symbol FNBT
 contract FNBToken is ERC20 {
+    /**
+     * @notice Deploys the FNBToken contract and mints the initial supply
+     * @param initialSupply The total number of tokens to mint to the deployer account
+     * @dev Pass supply with 18 decimal precision (e.g., 1000000000000000000000000 for 1M tokens)
+     */
     constructor(uint256 initialSupply) ERC20("FNB Token", "FNBT") {
+        // Mint the total token supply directly to the address deploying this contract
         _mint(msg.sender, initialSupply);
     }
 }
--- 02-uniswap-v4/packages/hardhat/contracts/PNPToken.sol
+++ 02-uniswap-v4/packages/hardhat/contracts/PNPToken.sol
@@ -6,7 +6,13 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 /// @title PNPToken
 /// @notice ERC20 token representing Pick n Pay points with symbol PNPT
 contract PNPToken is ERC20 {
+    /**
+     * @notice Deploys the PNPToken contract and mints the initial supply
+     * @param initialSupply The total number of tokens to mint to the deployer account
+     * @dev Pass supply with 18 decimal precision (e.g., 1000000000000000000000000 for 1M tokens)
+     */
     constructor(uint256 initialSupply) ERC20("PNP Token", "PNPT") {
+        // Mint the total token supply directly to the address deploying this contract
         _mint(msg.sender, initialSupply);
     }
 }
